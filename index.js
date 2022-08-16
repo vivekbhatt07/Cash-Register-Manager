3d02da1d79488ec09fefb4b1dacb754e3699a53b
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-16 20:39:29 +0530

    add: bonus exercise

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const cashGiven = document.querySelector("#cash_given");
 const errorMessage = document.querySelector(".error_message");
 const numNotes = document.querySelectorAll(".num_notes");
 const checkButton = document.querySelector(".check_btn");
+const nextBtn = document.querySelector(".next_btn");
+const cashBox = document.querySelector(".cash");
+const CheckBtnWrap = document.querySelector(".check_btn_wrap");
+const returnChange = document.querySelector(".return_change");
 
 const denominations = [2000, 500, 100, 20, 10, 5, 1];
+
+nextBtn.addEventListener("click", function showCashGiven() {
+  errorMessageHideHandler();
+  if (billAmount.value === "") {
+    errorMessage.innerText = errorMessageShowHandler("Invalid Value");
+  } else {
+    cashBox.style.display = "block";
+    nextBtn.style.display = "none";
+    CheckBtnWrap.style.display = "block";
+  }
+});
 
 checkButton.addEventListener("click", function clickHandler() {
   errorMessageHideHandler();
   if (billAmount.value >= 0) {
     if (cashGiven.value >= billAmount.value) {
+      returnChange.style.display = "flex";
       const amountReturn = cashGiven.value - billAmount.value;
       numNotesReturn(amountReturn);
     } else {
